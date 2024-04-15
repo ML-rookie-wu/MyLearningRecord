ed71317ffa17222e9aa384a97752abf1f93db6e4
ML-rookie-wu <1437578081@qq.com>
2024-04-15 14:05:01 +0800

    完善代码

--- RegressionTest/htpy/common/common_api.py
+++ RegressionTest/htpy/common/common_api.py
@@ -10,5 +10,10 @@ def get_specified_path(dir_name: str, cur_path: str):
     cur_dir_name = os.path.basename(cur_path)
     if cur_dir_name == dir_name:
         return cur_path
+
+    parent_dir = os.path.dirname(cur_path)
+    if parent_dir == cur_path:  # 如果已经到达了根目录，但是还没有找到目标文件夹，则返回None
+        return None
+
     return get_specified_path(dir_name, os.path.dirname(cur_path))
 
